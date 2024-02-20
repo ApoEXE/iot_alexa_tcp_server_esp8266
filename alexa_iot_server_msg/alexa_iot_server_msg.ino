63e6cdd3ade95ff0f93cfdd623e440b77ad670a8
Javier Pernia <javierpernia89@gmail.com>
2024-02-19 23:12:34 -0500

    added alexa control

--- alexa_iot_server_msg/alexa_iot_server_msg.ino
+++ alexa_iot_server_msg/alexa_iot_server_msg.ino
@@ -6,12 +6,17 @@
 #include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
-
+#include "fauxmoESP.h"
 #include "config.h"
 
 #define MAYOR 1
-#define MINOR 0
+#define MINOR 1
 #define PATCH 0
+#define WIFI_SSID "JAVI"
+#define WIFI_PASS "xavier1234"
+#define DEVICE "sala"
+
+fauxmoESP fauxmo;
 
 long int counter = 0;
 
@@ -87,12 +92,12 @@ static void handleNewClient(void* arg, AsyncClient* client) {
 
 void setup() {
   pinMode(BUILTIN_LED, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(BUILTIN_LED, HIGH);
   Serial.begin(115200);
   delay(20);
   Serial.println("ESP8266 Server");
   WiFi.mode(WIFI_STA);
-  WiFi.begin("TP-JAVI", "xavier1234");  // change it to your ussid and password
+  WiFi.begin(WIFI_SSID, WIFI_PASS);  // change it to your ussid and password
   while (WiFi.waitForConnectResult() != WL_CONNECTED) {
     Serial.println("Connection Failed! Rebooting...");
     delay(5000);
@@ -100,6 +105,27 @@ void setup() {
   }
   Serial.printf("[WIFI] STATION Mode, SSID: %s, IP address: %s\n", WiFi.SSID().c_str(), WiFi.localIP().toString().c_str());
   initOTA();
+  fauxmo.onSetState([](unsigned char device_id, const char* device_name, bool state, unsigned char value) {
+    // Callback when a command from Alexa is received.
+    // You can use device_id or device_name to choose the element to perform an action onto (relay, LED,...)
+    // State is a boolean (ON/OFF) and value a number from 0 to 255 (if you say "set kitchen light to 50%" you will receive a 128 here).
+    // Just remember not to delay too much here, this is a callback, exit as soon as possible.
+    // If you have to do something more involved here set a flag and process it in your main loop.
+
+    Serial.printf("[MAIN] Device #%d (%s) state: %s value: %d\n", device_id, device_name, state ? "ON" : "OFF", value);
+
+    // Checking for device_id is simpler if you are certain about the order they are loaded and it does not change.
+    // Otherwise comparing the device_name is safer.
+
+    if (strcmp(device_name, DEVICE) == 0) {
+      digitalWrite(BUILTIN_LED, state ? HIGH : LOW);
+    }
+  });
+  fauxmo.createServer(true);  // not needed, this is the default value
+  fauxmo.setPort(80);         // This is required for gen3 devices
+  fauxmo.enable(true);        // Disabling it will prevent the devices from being discovered and switched
+  fauxmo.addDevice(DEVICE);
+
   AsyncServer* server = new AsyncServer(TCP_PORT);  // start listening on tcp port 7050
   server->onClient(&handleNewClient, server);
   server->begin();
@@ -108,6 +134,16 @@ void setup() {
 void loop() {
   ArduinoOTA.handle();
   DNS.processNextRequest();
+  fauxmo.handle();
+
+  // This is a sample code to output free heap every 5 seconds
+  // This is a cheap way to detect memory leaks
+  static unsigned long last = millis();
+  if (millis() - last > 5000) {
+    last = millis();
+    ESP.getFreeHeap();
+    //Serial.printf("[MAIN] Free heap: %d bytes\n", ESP.getFreeHeap());
+  }
 }
 
 
