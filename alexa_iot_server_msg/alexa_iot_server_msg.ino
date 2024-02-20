aad5454c4c2014d129345ab481d314b9e13d5e3c
Javier Pernia <javierpernia89@gmail.com>
2024-02-19 23:03:51 -0500

    added cmd V for version and T for time lapsed

--- alexa_iot_server_msg/alexa_iot_server_msg.ino
+++ alexa_iot_server_msg/alexa_iot_server_msg.ino
@@ -3,7 +3,15 @@
 #include <DNSServer.h>
 #include <vector>
 
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
 #include "config.h"
+
+#define MAYOR 1
+#define MINOR 0
+#define PATCH 0
 
 long int counter = 0;
 
@@ -13,16 +21,7 @@ static DNSServer DNS;
 static std::vector<AsyncClient*> clients;  // a list to hold all clients
 
 
-void blink() {
-  for (int i = 0; i < 5; i++) {
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(100);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(100);
-  }
-  digitalWrite(LED_BUILTIN, HIGH);
-}
-
+void initOTA();
 
 /* clients events */
 static void handleError(void* arg, AsyncClient* client, int8_t error) {
@@ -35,16 +34,28 @@ static void handleData(void* arg, AsyncClient* client, void* data, size_t len) {
 
   Serial.write((uint8_t*)data, len);
 
-  
+
 
   // reply to client
-  if (strcmp((char*)data, "A") == 0) {
-  
+  if (strcmp((char*)data, "T") == 0) {
+
     if (client->space() > 32 && client->canSend()) {
       char reply[32];
-      uint32_t seconds =  (uint32_t)(millis()/1000);
+      uint32_t seconds = (uint32_t)(millis() / 1000);
       Serial.println(seconds);
-      sprintf(reply, "time %d", seconds);
+      sprintf(reply, "%d\n", seconds);
+      client->add(reply, strlen(reply));
+      client->send();
+    }
+  }
+  // reply to client
+  if (strcmp((char*)data, "V") == 0) {
+
+    if (client->space() > 32 && client->canSend()) {
+      char reply[32];
+      uint32_t seconds = (uint32_t)(millis() / 1000);
+      Serial.println(seconds);
+      sprintf(reply, "v%d.%d.%d\n", MAYOR, MINOR, PATCH);
       client->add(reply, strlen(reply));
       client->send();
     }
@@ -82,17 +93,68 @@ void setup() {
   Serial.println("ESP8266 Server");
   WiFi.mode(WIFI_STA);
   WiFi.begin("TP-JAVI", "xavier1234");  // change it to your ussid and password
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
   }
   Serial.printf("[WIFI] STATION Mode, SSID: %s, IP address: %s\n", WiFi.SSID().c_str(), WiFi.localIP().toString().c_str());
-
+  initOTA();
   AsyncServer* server = new AsyncServer(TCP_PORT);  // start listening on tcp port 7050
   server->onClient(&handleNewClient, server);
   server->begin();
 }
 
 void loop() {
+  ArduinoOTA.handle();
   DNS.processNextRequest();
+}
+
+
+void initOTA() {
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  // ArduinoOTA.setHostname("myesp8266");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else {  // U_FS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
 }
